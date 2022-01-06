eb863c61612e26557859f05f3b1707e03eb457ca
SandraMey <sandra.meyniel@gmail.com>
2022-01-06 11:34:40 +0100

    correction test + add insertTrack

--- src/models/tracks.js
+++ src/models/tracks.js
@@ -2,9 +2,18 @@ const connexion = require('../connexion');
 
 const findAll = () => connexion.promise().query('SELECT * FROM tracks');
 
-const findOneById = (id) => connexion.promise().query('SELECT * FROM tracks WHERE id = ?', [id])
+const findOneById = (id) => connexion.promise().query('SELECT * FROM tracks WHERE id = ?', [id]);
+
+const insertTracks = ({
+    title,
+    youtube_url
+}, id_album) => connexion.promise().query('INSERT INTO tracks (title, youtube_url) VALUES (?, ?)',[
+    title,
+    youtube_url
+])
 
 module.exports = {
     findAll,
-    findOneById
+    findOneById,
+    insertTracks
 }
--- src/routes/tracks.js
+++ src/routes/tracks.js
@@ -1,5 +1,5 @@
 const tracksRouter = require('express').Router();
-const { findAll, findOneById } = require('../models/tracks');
+const { findAll, findOneById, insertTracks } = require('../models/tracks');
 
 tracksRouter.get('/', async (req, res) => {
     const [[tracks]] = await findAll()
@@ -13,6 +13,15 @@ tracksRouter.get('/:id', async (req, res) => {
     if (track) {
         res.json(track);
     } res.status(404).json();
+});
+
+tracksRouter.post('/', async (req, res) => {
+    const [{insertId: id}] = await insertTracks(req.body);
+    const newTracks = req.body;
+    res.status(201).json({
+        id,
+        ...newTracks
+    }, id_album);
 });
 
 module.export = tracksRouter;
